5f7186e6d3d596a164206b66239f5719e3870732
Alvaro Lupa <alvaro.lupa@gmail.com>
2020-04-06 16:03:27 -0400

    QR reader http and geo terminated

---
+++ lib/src/models/scan_model.dart
@@ -0,0 +1,35 @@
+import 'package:latlong/latlong.dart';
+
+class ScanModel {
+  int id;
+  String tipo;
+  String valor;
+
+  ScanModel({
+    this.id,
+    this.tipo,
+    this.valor,
+  }) {
+    this.tipo = (this.valor.contains('http')) ? 'http' : 'geo';
+  }
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id   : json["id"],
+        tipo : json["tipo"],
+        valor: json["valor"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id"   : id,
+        "tipo" : tipo,
+        "valor": valor,
+      };
+  
+  LatLng getLatLng(){
+    final latlng = valor.substring(4).split(',');
+    final lat = double.parse(latlng[0]);
+    final lng = double.parse(latlng[1]);
+
+    return LatLng(lat, lng);
+  }
+}
---
+++ lib/src/pages/mapa_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
+
+class MapaPage extends StatefulWidget {
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  final map = MapController();
+
+  String tipoMapa = 'streets';
+
+  @override
+  Widget build(BuildContext context) {
+
+    final ScanModel scan = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coordenadas QR'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.my_location),
+            onPressed: () => map.move(scan.getLatLng(), 15),
+          )
+        ],
+      ),
+      body: Center(
+        child: _crearFlutterMap(scan)
+      ),
+      floatingActionButton: _crearBotonFlotante(context),
+    );
+  }
+
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      mapController: map,
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 15
+      ),
+      layers: [
+        _crearMapa(),
+        _crearMarcadores(scan),
+      ],
+    );
+  }
+
+  _crearMapa() {
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1IjoiYWx1cGEiLCJhIjoiY2s4b3FhbWp5MWJ4dDNxcGdhZmh2YTV2ayJ9.73Gp17i8yTE1cQnu1XAhXQ',
+        'id': 'mapbox.$tipoMapa' // streets, dark, light, outdoors, satellite
+      }
+    );
+  }
+
+  _crearMarcadores(ScanModel scan) {
+    return MarkerLayerOptions(
+      markers: <Marker>[
+        Marker(
+          width: 100.0,
+          height: 100.0,
+          point: scan.getLatLng(),
+          builder: (context) => Container(
+            child: Icon(
+              Icons.location_on, 
+              size: 50.0,
+              color: Theme.of(context).primaryColor,
+              ),
+          )
+        ),
+      ]
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: (){
+        // streets, dark, light, outdoors, satellite
+        switch(tipoMapa){
+          case 'streets' : tipoMapa = 'dark'; break;
+          case 'dark'    : tipoMapa = 'light'; break;
+          case 'light'   : tipoMapa = 'outdoors'; break;
+          case 'outdoors': tipoMapa = 'satellite'; break;
+          default        : tipoMapa = 'streets';
+        }
+
+        setState((){});
+      },
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+}
